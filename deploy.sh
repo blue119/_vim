a4195a5a5b7b1c70c510f77f388169ce78be6bb2
Yao-Po Wang <blue119@gmail.com>
2012-12-16 22:00:11 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bain/sh
+
+fs+=(.vim)
+fs+=(.vimrc)
+
+date=`date +"%s"`
+# move same filename to end up with _data.bak for backup old file.
+git submodule
+for f in ${fs[*]}
+do
+	if [[ ! -L ${HOME}/${f} ]]; then
+		if [[ -f ${HOME}/${f} ]] || [[ -d ${HOME}/${f} ]]; then
+			echo "backup ${HOME}/${f} => ${HOME}/${f}_${date}_bak"
+			mv ${HOME}/${f} ${HOME}/${f}_${date}_bak;
+		fi
+	else
+		echo "delete symbolic link ${HOME}/${f}"
+		rm ${HOME}/${f}
+	fi
+		echo "link ${PWD}/${f} => ${HOME}/${f}"
+		ln -s ${PWD}/${f} ${HOME}/${f}
+done
+
