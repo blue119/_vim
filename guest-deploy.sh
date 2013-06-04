95bb6d69963d5332e680e8a14c622bb7e809b558
Yao-Po Wang <blue119@gmail.com>
2013-06-05 00:06:19 +0800

    new script for guest depoly

---
+++ guest-deploy.sh
@@ -0,0 +1,27 @@
+#!/bain/sh
+
+fs+=(.vim)
+fs+=(.vimrc)
+
+sed -i 's|git@github.com:blue119|git://github.com/blue119|g' .gitmodules
+sed -i 's|git@github.com:blue119|git://github.com/blue119|g' .git/config
+
+date=`date +"%s"`
+# move same filename to end up with _data.bak for backup old file.
+git submodule init
+git submodule update
+for f in ${fs[*]}
+do
+	if [[ ! -L ${HOME}/${f} ]]; then
+		if [[ -f ${HOME}/${f} ]] || [[ -d ${HOME}/${f} ]]; then
+			echo "backup ${HOME}/${f} => ${HOME}/${f}_${date}_bak"
+			mv ${HOME}/${f} ${HOME}/${f}_${date}_bak;
+		fi
+	else
+		echo "delete symbolic link ${HOME}/${f}"
+		rm ${HOME}/${f}
+	fi
+		echo "link ${PWD}/${f} => ${HOME}/${f}"
+		ln -s ${PWD}/${f} ${HOME}/${f}
+done
+
